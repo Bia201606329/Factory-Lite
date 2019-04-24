dc0df1f8ac1e3f99240c1ab80851a9db2a5f9f66
joaoppires <47816071+joaoppires@users.noreply.github.com>
2019-04-24 21:15:17 +0100

    Add files via upload

--- prova1.ino
+++ prova1.ino
@@ -124,7 +124,7 @@ void setup()
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
 
-  UsingSimulator = 1;
+  UsingSimulator = 1;                                                 //SIMULADOR(1) VS. REAL(0)
 
 }
 
@@ -180,7 +180,7 @@ void readIRSensors(void)
   }      
 }
 
-uint32_t tis, aux=0;
+uint32_t tis, tis_aux=0;                                                                //VARIAVEIS
 uint32_t current, previous, interval = 40000UL;
 uint8_t cont_arm=0;
 
@@ -260,8 +260,14 @@ void setState(byte new_state)
   IRLine.crosses=0;
 }
 
+#define Varm 30
+#define Vmed 100
+#define Vmin 60
+#define Vmax 200
+#define kapa -4.0
 void control(void)
 {
+
     /*if (robot.state == 1 && TouchSwitch) {
       setState(2);
     } else if(robot.state == 2 && tis > 400) {
@@ -320,30 +326,34 @@ void control(void)
       if (tis < 2000) moveRobot(-40, 0);
       else moveRobot(0, 0);
     }  */
-
+  //tentativa 1
+  /*
       if (robot.state==1 && TouchSwitch) setState(2);
       else if (robot.state==2 && IRLine.crosses >= 1) setState(4); //volta para trás até detetar a direita
       //else if (robot.state==3 && tis > 750 && IRLine.total > 1500) setState(4); //estar centrado
-      else if(robot.state==4 && IRLine.crosses>=7-cont_arm) setState(5); //andar até o cruzamento
+      else if(robot.state==4 && IRLine.crosses>=(6-cont_arm)) setState(5); //andar até o cruzamento
       else if (robot.state==5 && tis>=1000) setState(6); //ultrapassar o cruzamento
       else if (robot.state==6 && IRLine.crosses>=3+cont_arm) setState(7); //andar até ao armazem certo
-      else if (robot.state==7 && tis>2000) setState(8); //entrar no armazem
+      else if (robot.state==7 && tis>=3000) setState(8); //entrar no armazem
       else if (robot.state==8 && IRLine.crosses>=1){  //recua até detetar o cruzamento
         setState(9); 
         cont_arm++;
       }
       else if (robot.state==9 && tis > 750 && IRLine.total > 1500) setState(10); //roda até estar centrado
+      else if (robot.state==10 && cont_arm==4) setState(0); //termino
       else if (robot.state==10 && IRLine.crosses>=cont_arm) setState(11); //anda até detetar intersecção à direita
-      else if (robot.state==11 && IRLine.crosses>=3) setState(12); //segue até à interseção das peças
+      else if (robot.state==11 && IRLine.crosses>=4) setState(12); //segue até à interseção das peças
       else if (robot.state==12 && IRLine.crosses>=cont_arm) setState(1); //anda até à entrada correta
-
+      
       
       if (robot.state == 0) {         // Robot Stoped            
         robot.solenoid_state = 0;
         moveRobot(0, 0);
+        
       } else if (robot.state == 1) {
-        robot.solenoid_state = 0;
-        followLineLeft(60, -1.0);
+        robot.solenoid_state = 1;
+        if(cont_arm==0) followLineLeft(200, -2.0);
+        else followLineLeft(60, -2.0);
       } else if (robot.state == 2) {
         robot.solenoid_state = 1;
         moveRobot(-60, 0);
@@ -352,34 +362,141 @@ void control(void)
         moveRobot(0, -50); 
       } else if (robot.state == 4) {
         robot.solenoid_state = 1;
-        if(tis<1500) followLineRight(40, -1.0);  
-        else  followLineRight(60, -1.0);
+        if(tis<1000) followLineRight(40, -2.0);
+        else followLineRight(60, -2.0);
       } else if (robot.state == 5) {
         robot.solenoid_state = 1;
         moveRobot(60, 0);
       } else if (robot.state == 6) {
         robot.solenoid_state = 1;
-        followLineLeft(60, -1.0);
+        followLineLeft(60, -2.0);
       } else if (robot.state == 7) {
         robot.solenoid_state = 1;
-        followLineRight(40, -1.0);
+        followLineRight(30, -2.0);
       } else if (robot.state == 8) {
         robot.solenoid_state = 0;
         moveRobot(-60, 0);
       } else if (robot.state == 9) {
-       robot.solenoid_state = 0;
+        robot.solenoid_state = 0;
         moveRobot(0, -50);
       } else if (robot.state == 10) {
         robot.solenoid_state = 0;
-        followLineRight(60, -1.0);
+        if(tis<1500) followLineRight(40, -2.0);
+        else followLineRight(60, -2.0);
       } else if (robot.state == 11) {
         robot.solenoid_state = 0;
-        followLineLeft(60, -1.0);
+        followLineLeft(60, -2.0);
       } else if (robot.state == 12) {
         robot.solenoid_state = 0;
-        followLineRight(60, -1.0);
+        followLineRight(40, -2.0);
       }    
-  
+      */
+      //tentativa2
+
+      if (robot.state==1 && TouchSwitch) setState(2);
+      else if (robot.state==2 && IRLine.crosses == 1) setState(3); //volta para trás até detetar a direita
+      else if (robot.state==3 && tis > 1500 && IRLine.total > 1500) setState(4); //estar centrado
+      else if(robot.state==4 && IRLine.crosses==5) setState(5); //speed até à curva
+      else if (robot.state==5 && ((tis>=3000 && cont_arm==0)||(tis>=1750 && cont_arm>0))) setState(6); //entrar no armazem
+      else if (robot.state==6 && IRLine.crosses==1){  //recua até detetar o cruzamento
+        setState(7); 
+        cont_arm++;
+      } else if (robot.state==7 && cont_arm==4) setState(0); //termino
+      else if (robot.state==7 && IRLine.crosses==(1+cont_arm)) setState(8); //antes do cruzamento
+      else if (robot.state==8 && tis>=1100) setState(9); //cruzamento
+      else if (robot.state==9 && IRLine.crosses==(6-cont_arm)) setState(10);
+      else if (robot.state==10 && TouchSwitch) setState(11);
+      else if (robot.state==11 && IRLine.crosses==1) setState(12);
+      else if (robot.state==12 && IRLine.crosses==(5-cont_arm)) setState(13); //andar até o cruzamento 
+      else if (robot.state==13 && tis>=1100) setState(14); //ultrapassar o cruzamento
+      else if (robot.state==14 && IRLine.crosses==3+cont_arm) setState(15); //andar até ao armazem certo | quando cont_arm=3 posso passar state=5 quando crosses==5
+
+      else if (robot.state==15 && tis>=300) setState(16);
+      else if (robot.state==16 && tis>=750 && IRLine.total > 1500) setState(5);
+      
+      //else if(robot.state==4 && IRLine.crosses>=(6-cont_arm)) setState(5); //andar até o cruzamento
+      //else if (robot.state==5 && tis>=1000) setState(6); //ultrapassar o cruzamento
+      //else if (robot.state==6 && IRLine.crosses>=3+cont_arm) setState(7); //andar até ao armazem certo
+      //else if (robot.state==7 && tis>=3000) setState(8); //entrar no armazem
+      //else if (robot.state==8 && IRLine.crosses>=1){  //recua até detetar o cruzamento
+      //  setState(9); 
+      //  cont_arm++;
+      //}
+      //else if (robot.state==9 && tis > 750 && IRLine.total > 1500) setState(10); //roda até estar centrado
+      //else if (robot.state==10 && cont_arm==4) setState(0); //termino
+      //else if (robot.state==10 && IRLine.crosses>=cont_arm) setState(11); //anda até detetar intersecção à direita
+      //else if (robot.state==11 && IRLine.crosses>=4) setState(12); //segue até à interseção das peças
+      //else if (robot.state==12 && IRLine.crosses>=cont_arm) setState(1); //anda até à entrada correta
+      
+      
+      if (robot.state == 0) {         // Robot Stoped            
+        robot.solenoid_state = 0;
+        moveRobot(0, 0);
+      } else if (robot.state == 1) {
+        robot.solenoid_state = 1;
+        followLineLeft(Vmed, kapa);
+      } else if (robot.state == 2 || robot.state==11) {
+        robot.solenoid_state = 1;
+        moveRobot(-Vmed, 0);
+      } else if (robot.state == 3) {
+        robot.solenoid_state = 1;
+        moveRobot(0, 50); 
+      } else if (robot.state == 4) {
+        robot.solenoid_state = 1;
+        followLineRight(Vmed-40*(IRLine.crosses==5), -2.0);
+      } else if (robot.state == 5) {
+        robot.solenoid_state = 1;
+        followLineRight(Varm + 20*(cont_arm>0), -2.0);
+      } else if (robot.state == 6) {
+        robot.solenoid_state = 0;
+        moveRobot(-Vmed, 0);
+      } else if (robot.state == 7) {
+        robot.solenoid_state = 0;
+        //if(tis<1000) followLineRight(Vmin, kapa);
+        //else followLineRight(Vmed, kapa);
+        followLineRight(Vmed, kapa);
+      } else if (robot.state == 8) {
+        robot.solenoid_state = 0;
+        moveRobot(Vmed, 0);
+      } else if (robot.state == 9) {
+        robot.solenoid_state = 0;
+        followLineLeft(Vmed, kapa);
+      } else if (robot.state == 10) {
+        robot.solenoid_state = 1;
+        followLineRight(Vmed, kapa);
+      } else if (robot.state == 12) {
+        robot.solenoid_state = 1;
+        //if(tis<1000) followLineRight(Vmin, kapa);
+        //else followLineRight(Vmed, kapa);
+        followLineRight(Vmed, kapa);
+      } else if (robot.state == 13 && robot.state==15) {
+        robot.solenoid_state = 1;
+        moveRobot(Vmed, 0);
+      } else if (robot.state == 14) {
+        robot.solenoid_state = 1;
+        followLineLeft(Vmed-40*(IRLine.crosses==6), kapa);
+      } else if(robot.state==16){
+        robot.solenoid_state = 1;
+        moveRobot(0, -50);
+      }
+
+
+      
+      //teste para contar crosses
+      /*if (robot.state == 1) {  // Go: Get first box
+        robot.solenoid_state = 0;
+        followLineLeft(60, -2.0);
+      } else if(robot.state==2){
+        robot.solenoid_state = 0;
+        followLineRight(60, -2.0);
+      } else if(robot.state==3){
+        robot.solenoid_state = 0;
+        followLineRight(0, 0);
+      }
+
+
+      if (robot.state == 1 && IRLine.crosses==3)  setState(2); //contar crosses
+      else if (robot.state == 2 && IRLine.crosses==2)  setState(3);*/
 }
 
 void loop(void)
@@ -389,6 +506,8 @@ void loop(void)
   } else {
     real_loop();
   }
+  //real_loop();
+  //setState(1);
 }
 
 
@@ -473,6 +592,11 @@ void real_loop(void)
        Serial.print(IRLine.IR_values[c]);
        Serial.print(" ");
     }
+
+    Serial.println();
+    Serial.print(IRLine.total);
+    Serial.print("  ");
+    
     Serial.print(" PosR: ");
     Serial.print(IRLine.pos_right);
 
